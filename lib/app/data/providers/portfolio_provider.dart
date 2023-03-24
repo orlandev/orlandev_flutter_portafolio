6b0d800252f792a632feb7d9a4837b80e00932d4
orlandev <onovas05@gmail.com>
2023-03-24 15:12:49 -0400

    Add new provider

--- lib/app/data/providers/portfolio_provider.dart
+++ lib/app/data/providers/portfolio_provider.dart
@@ -7,9 +7,8 @@ class PortfolioProvider extends GetConnect {
   void onInit() {
     httpClient.defaultDecoder = (map) {
       if (map is Map<String, dynamic>) return Portfolio.fromJson(map);
-      if (map is List) {
+      if (map is List)
         return map.map((item) => Portfolio.fromJson(item)).toList();
-      }
     };
     httpClient.baseUrl = 'YOUR-API-URL';
   }
