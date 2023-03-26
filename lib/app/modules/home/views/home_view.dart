b8886c1b2aba266720e87d333bff51f3f035bc46
Orlando N. Rodriguez <onovas05@gmail.com>
2023-03-26 00:57:37 -0400

    Some changes in teh UI

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -10,26 +8,40 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Obx(() => Text(
-                  'HomeView is working: ${controller.currentPortfolio.value?.portfolio?.info?.name ?? 'EMPTY'}',
-                  style: const TextStyle(fontSize: 20),
-                )),
-            MaterialButton(
-              child: const Text('HomeView is working'),
-              onPressed: () {
-
-              },
-            ),
-          ],
-        ),
-      ),
+      body: Column(children: [
+        Expanded(
+            flex: 1,
+            child: SizedBox(
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        radius: 55,
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage:
+                              NetworkImage('https://picsum.photos/id/237/200/300'),
+                        ),
+                      ),
+                      Column(children: const [
+                        Text("Orlando N. Rodriguez"),
+                      ],)
+                    ],
+                  ),
+                ),
+              ),
+            )),
+        Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.blue,
+            )),
+      ]),
     );
   }
 }
