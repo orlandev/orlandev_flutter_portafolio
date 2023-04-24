db155c9f064eb2abd3e8a73d1b903b4d5071c5c0
orlandev <onovas05@gmail.com>
2023-04-24 15:52:56 -0400

    Restructure the project and add Parallax dependencies

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,19 +6,52 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
+    return const Scaffold(
+      body: Center(
         child: Text(
           'HomeView is working',
           style: TextStyle(fontSize: 20),
         ),
       ),
     );
+  }
+}
+
+class HomeParallaxContainer extends StatelessWidget {
+  const HomeParallaxContainer(
+      {Key? key, required this.front, required this.back})
+      : super(key: key);
+
+  final Widget front;
+  final Widget back;
+
+  @override
+  Widget build(BuildContext context) {
+    return Parallax(
+        sensors: ParallaxSensor.accelerometer,
+        layers: [
+          Layer(
+            sensitivity: 1,
+            image: NetworkImage('https://example.com/background.png'),
+            preventCrop: true,
+            imageBlurValue: 5,
+          ),
+          Layer(
+            sensitivity: 7,
+            image: AssetImage('assets/middle_layer.png'),
+            imageHeight: 500,
+            imageFit: BoxFit.fitHeight,
+          ),
+          Layer(
+            sensitivity: 12,
+            child: Text('Topmost layer'),
+          ),
+        ]
+        child: Text('Page body here'),)
+    ,
+
   }
 }
